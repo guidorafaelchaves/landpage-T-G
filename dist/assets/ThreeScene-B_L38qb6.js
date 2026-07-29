e7c8f4ba26f1d5eeff1c334f3e59f7440c5a24c1
guidorafaelchaves <65878083+guidorafaelchaves@users.noreply.github.com>
2026-07-29 04:43:30 -0300

    deploy: fixar previa temporaria

--- dist/assets/ThreeScene-B_L38qb6.js
+++ dist/assets/ThreeScene-B_L38qb6.js
@@ -1,4 +1,4 @@
-import{r as Qs,j as wi}from"./main-Bui3L2hO.js";/**
+import{r as Qs,j as wi}from"./main-Utx21zFt.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
