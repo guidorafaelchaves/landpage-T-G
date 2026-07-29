bcd5cd249e36e7e4e7895afe052a4c1178a1eb00
guidorafaelchaves <65878083+guidorafaelchaves@users.noreply.github.com>
2026-07-29 04:38:25 -0300

    deploy: atualizar previa temporaria

--- dist/assets/ThreeScene-BTEbn4Y-.js
+++ dist/assets/ThreeScene-BTEbn4Y-.js
@@ -1,4 +1,4 @@
-import{r as Qs,j as wi}from"./main-Da-aYPVe.js";/**
+import{r as Qs,j as wi}from"./main-Bui3L2hO.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
